61edf2e6c7965ca3543bd7d0744feaccc37a1437
Pixirora <rina@pixirora.dev>
2024-01-02 22:11:37 -0500

    Personalize mod metadata & remove QFAPI dependency

---
+++ src/main/java/net/pixaurora/kitty_music/impl/KittyMusic.java
@@ -0,0 +1,10 @@
+package net.pixaurora.kitty_music.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KittyMusic {
+	public static final String MOD_ID = "kitty_music";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
