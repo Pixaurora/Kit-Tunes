700a34bb9468c9c0e3b7b2fa8f3fcb86c79eda53
Pixirora <rina@pixirora.dev>
2024-06-04 20:07:42 -0400

    Implement own hex format function
    This is one of the last things needed before
    Java 8 support could be added.

--- subprojects/core/src/main/java/net/pixaurora/kit_tunes/impl/network/Encryption.java
+++ subprojects/core/src/main/java/net/pixaurora/kit_tunes/impl/network/Encryption.java
@@ -3,10 +3,15 @@ package net.pixaurora.kit_tunes.impl.network;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HexFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Encryption {
 	public static String signMd5(String input) {
+		return bytesToHex(getMd5Digest(input));
+	}
+
+	public static byte[] getMd5Digest(String input) {
 		MessageDigest digester;
 		try {
 			digester = MessageDigest.getInstance("MD5");
@@ -15,6 +20,27 @@ public class Encryption {
 		}
 
 		digester.update(input.getBytes(StandardCharsets.UTF_8));
-		return HexFormat.of().formatHex(digester.digest());
+
+		return digester.digest();
+	}
+
+	public static String bytesToHex(byte[] bytes) {
+		List<Integer> hexDigits = new ArrayList<>();
+
+		for (byte rawByte : bytes) {
+			int processedByte = Byte.toUnsignedInt(rawByte);
+
+			// Example byte: 11 01 10 00
+			hexDigits.add(processedByte >> 4); // Grabs the part that is 11 01
+			hexDigits.add(processedByte % 16); // Grabs the part that is 10 00
+		}
+
+		String output = "";
+
+		for (int i : hexDigits) {
+			output += Integer.toHexString(i);
+		}
+
+		return output;
 	}
 }
