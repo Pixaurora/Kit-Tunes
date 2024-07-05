64f769f4274865bf132b3f5871965ec601bf3fbe
Pixirora <rina@pixirora.dev>
2024-07-05 16:13:46 -0400

    Fix music polling
    It seems like sometimes the sound event for
    "track started" happens before the channel is
    actually created. Thus, we simply wait until the
    channel exists and then begin polling as before.

--- subprojects/sound_events/r1.20.4/src/main/java/net/pixaurora/kit_tunes/impl/MusicPolling.java
+++ subprojects/sound_events/r1.20.4/src/main/java/net/pixaurora/kit_tunes/impl/MusicPolling.java
@@ -7,37 +7,50 @@ import java.util.Optional;
 
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.ChannelAccess;
+import net.minecraft.client.sounds.ChannelAccess.ChannelHandle;
 import net.pixaurora.kit_tunes.api.music.Track;
 import net.pixaurora.kit_tunes.impl.music.progress.PolledListeningProgress;
 import net.pixaurora.kit_tunes.impl.music.progress.SongProgressTracker;
 
 public class MusicPolling {
+    private static List<TrackToPoll> TRACKS_TO_POLL = new ArrayList<>();
     private static List<PolledTrack> POLLED_TRACKS = new ArrayList<>();
 
     public static void trackStarted(SoundInstance sound, Track track) {
-        POLLED_TRACKS.add(new PolledTrack(sound, track, new PolledListeningProgress()));
+        TRACKS_TO_POLL.add(new TrackToPoll(sound, track));
     }
 
     public static void pollTrackProgress(Map<SoundInstance, ChannelAccess.ChannelHandle> instanceToChannel) {
-        List<PolledTrack> finishedTracks = new ArrayList<>();
+        TRACKS_TO_POLL.removeIf((track) -> {
+            Optional<ChannelHandle> channel = Optional.ofNullable(instanceToChannel.get(track.key));
 
-        for (PolledTrack track : POLLED_TRACKS) {
-            var channelHandle = Optional.ofNullable(instanceToChannel.get(track.key()));
+            if (channel.isPresent()) {
+                POLLED_TRACKS.add(new PolledTrack(channel.get(), track.track, new PolledListeningProgress()));
 
-            if (channelHandle.isPresent()) {
-                channelHandle.get()
-                        .execute(channel -> track.progress().measureProgress((SongProgressTracker) (Object) channel));
+                return true;
             } else {
-                finishedTracks.add(track);
+                return false;
             }
-        }
+        });
 
-        for (PolledTrack finishedTrack : finishedTracks) {
-            KitTunesEvents.onTrackEnd(finishedTrack.track, finishedTrack.progress);
-            POLLED_TRACKS.remove(finishedTrack);
-        }
+        POLLED_TRACKS.removeIf((track) -> {
+            if (track.channel.isStopped()) {
+                KitTunesEvents.onTrackEnd(track.track, track.progress);
+
+                return true;
+            } else {
+                track.channel
+                        .execute(channel -> track.progress().measureProgress((SongProgressTracker) (Object) channel));
+
+                return false;
+            }
+        });
     }
 
-    private static record PolledTrack(SoundInstance key, Track track, PolledListeningProgress progress) {
+    private static record TrackToPoll(SoundInstance key, Track track) {
+
+    }
+
+    private static record PolledTrack(ChannelHandle channel, Track track, PolledListeningProgress progress) {
     }
 }
