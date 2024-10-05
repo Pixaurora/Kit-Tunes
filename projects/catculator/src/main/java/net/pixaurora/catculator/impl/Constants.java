ebd071f6e45be82a962b0acb20e4bafbd1204488
Pixirora <rina@pixirora.dev>
2024-10-05 09:51:29 -0400

    Cache natives to '.cache/kit_tunes' for easier understanding

--- projects/catculator/src/main/java/net/pixaurora/catculator/impl/Constants.java
+++ projects/catculator/src/main/java/net/pixaurora/catculator/impl/Constants.java
@@ -5,10 +5,11 @@ import org.quiltmc.loader.api.QuiltLoader;
 import java.nio.file.Path;
 
 public class Constants {
+    public static final String PARENT_MOD_ID = "kit_tunes";
     public static final String MOD_ID = "catculator";
 
     public static final String NATIVES_VERSION = "0.1.0";
     public static final String NATIVES_DIRECTORY_PROPERTY = "catculator.natives_path";
 
-    public static final Path NATIVES_CACHE_DIR = QuiltLoader.getCacheDir().resolve(MOD_ID);
+    public static final Path NATIVES_CACHE_DIR = QuiltLoader.getCacheDir().resolve(PARENT_MOD_ID);
 }
