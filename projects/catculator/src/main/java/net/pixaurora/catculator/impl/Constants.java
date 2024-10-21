e5c4cd88ed405c1ea9f5e3db39f660e6d5e96cc5
Pixirora <rina@pixirora.dev>
2024-10-20 21:23:48 -0400

    Move next version of natives to production

--- projects/catculator/src/main/java/net/pixaurora/catculator/impl/Constants.java
+++ projects/catculator/src/main/java/net/pixaurora/catculator/impl/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String PARENT_MOD_ID = "kit_tunes";
     public static final String MOD_ID = "catculator";
 
-    public static final String NATIVES_VERSION = "0.1.0";
+    public static final String NATIVES_VERSION = "0.2.0";
     public static final String NATIVES_DIRECTORY_PROPERTY = "catculator.natives_path";
 
     public static final Path NATIVES_CACHE_DIR = QuiltLoader.getCacheDir().resolve(PARENT_MOD_ID);
